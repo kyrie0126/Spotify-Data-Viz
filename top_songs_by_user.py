99f5109586f416913caab17791e168bd230ce583
Kyle Wong <kylewong@berkeley.edu>
2023-09-28 08:12:02 -0700

    adding changes

--- top_songs_by_user.py
+++ top_songs_by_user.py
@@ -2,29 +2,35 @@ import os
 import base64
 import requests
 import json
+import random
+import string
+import hashlib
+
 
 CLIENT_ID = os.environ.get('SPOTIFY_CLIENT_ID')
 CLIENT_SECRET = os.environ.get('SPOTIFY_CLIENT_SECRET')
-
-def get_token():
-    auth_string = f'{CLIENT_ID}:{CLIENT_SECRET}'
-    auth_bytes = auth_string.encode('utf-8')
-    auth_base64 = str(base64.b64encode(auth_bytes), 'utf-8')
-    
-    url = 'https://accounts.spotify.com/api/token'
-    headers = {
-        'Authorization': f'Basic {auth_base64}',
-        'Content-Type': 'application/x-www-form-urlencoded'
-        }
-    
-    data = {'grant_type': 'client_credentials'}
-    
-    result = requests.post(url, headers=headers, data=data)
-    json_result = json.loads(result.content)
-    token = json_result['access_token']
-    return token
+REDIRECT_URI = "http://localhost:5500/"
 
 
-def get_auth_header(token):
-    return {'Authorization': f'Bearer {token}'}
+# Code Verifier
+
+def generate_random_string(length):
+    """The PKCE authorization flow starts with the creation of a code verifier"""
+    characters = string.ascii_letters + string.digits
+    random_string = ''.join(random.choice(characters) for _ in range(length))
+    return random_string
+
+
+async def generate_code_challenge(code_verifier):
+    """We must hash the code using the SHA256 algorithm then convert it to base64 encoding"""
+    def base64encode(data):
+        encoded_bytes = base64.urlsafe_b64encode(data)
+        encoded_string = encoded_bytes.decode('utf-8').rstrip('=')
+        return encoded_string
+
+    encoder = hashlib.sha256()
+    encoder.update(code_verifier.encode('utf-8'))
+    digest = encoder.digest()
+
+    return base64encode(digest)
 
